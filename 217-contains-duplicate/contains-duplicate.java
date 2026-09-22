0de2aa7d066c8e8bde75239a96709368c1247cd7
Uttam Saini <144931964+Uttamsaini15@users.noreply.github.com>
2026-09-22 09:27:17 +0530

    Time: 25 ms (18.02%) | Memory: 93.1 MB (78.31%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,11 +3,10 @@ class Solution {
         HashSet<Integer> hs = new HashSet<>();
 
         for(int n: nums){
-            if(hs.contains(n)) return true;
             hs.add(n);
         }
 
-        return false;
+        return hs.size()<nums.length;
 
     }
 }
